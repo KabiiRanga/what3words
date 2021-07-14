dc2d428a5fa0b78919bfe9f8122b349185e21d01
Kabilraj Ranganathan <kabilraj.ranganathan@santander.co.uk>
2021-07-14 13:26:27 +0100

    newFunc: getw3w;getWarningMessage

--- cypress/integration/pages/searchPanel.js
+++ cypress/integration/pages/searchPanel.js
@@ -22,5 +22,31 @@ module.exports = {
       .then(() => {
         cy.get('[data-testid="ThreeWordAddress-Text"]').should('have.text', what3words)
       })
+  },
+  selectLocationFromResults: function (expLocation) {
+    this.getSearchResults()
+      .should(($list) => {
+        expect($list).to.have.length(5)
+      })
+      .each(($ele) => {
+        if ($ele.find('.SearchPanel-LocationLine1').text() === expLocation) {
+          $ele.click()
+          return false
+        }
+      }).then(() => {
+        cy.get('[data-testid="ThreeWordAddress-Text"]').should('not.have.text', '').invoke("text").should('match', /^([a-z]+\.[a-z]+\.[a-z]+)$/)
+      })
+  },
+  submitSearch: function () {
+    cy.get('[data-testid="SearchPanel-Input"]').type('{enter}')
+  },
+  printW3WAddress: function () {
+    cy.get('[data-testid="ThreeWordAddress-Text"]').as('w3w').should('not.have.text', '').invoke("text").then((text) => {
+      expect(text.split('.')).to.have.lengthOf(3)
+      cy.log('w3w address: ' + text)
+    })
+  },
+  getWarningMessage: function() {
+    return cy.get('.NotificationMessage-Label div').as('warningMessage').should('be.visible')
   }
 }
